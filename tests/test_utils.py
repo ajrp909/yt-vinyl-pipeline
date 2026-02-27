782de511b62ec4e01e081db851cd3a1498f412f3
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2026-02-27 23:34:47 +0000

    added test for unsuccessful api call

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import pytest
+
 from yt_vinyl.utils import get_api_key
 
 
@@ -8,3 +10,13 @@ def test_get_api_key_successful(mocker):
 
     assert result == "mocked_key"
     assert isinstance(result, str)
+
+
+def test_get_api_key_failed(mocker):
+
+    mocker.patch("yt_vinyl.utils.os.getenv", return_value=None)
+    with pytest.raises(ValueError) as exec_info:
+        get_api_key("test_string")
+
+    assert exec_info.type is ValueError
+    assert exec_info.value.args[0] == "API key field cannot be None"
