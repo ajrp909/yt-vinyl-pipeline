7dd426d11ac10f27b991de2c961e1347f59d7e40
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2026-02-27 23:00:00 +0000

    writing first tests

--- src/yt_vinyl/ingestion/youtube.py
+++ src/yt_vinyl/ingestion/youtube.py
@@ -1,8 +1,19 @@
-from yt_vinyl.utils import get_api_key
+from googleapiclient.discovery import Resource, build
 
 
-youtube_api_key = get_api_key("TEST_KEY")
+# youtube_api_key = get_api_key("YOUTUBE_API_KEY")
 
-print(youtube_api_key)
 
-# youtube = build("youtube", "v3", developerKey=load_dotenv())
+def youtube_client_object(api_key: str):
+    return build("youtube", "v3", developerKey=api_key)
+
+
+def get_video_ids(playlist_id: str, youtube_object: Resource) -> list:
+
+    video_object = (
+        youtube_object.playlistItems()
+        .list(part="contentDetails", playlistId=playlist_id, maxResults=50)
+        .execute()
+    )
+
+    return [item["contentDetails"]["videoId"] for item in video_object["items"]]
