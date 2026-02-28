318a883bfd2fc51b341b3035a17bf3cba3c3831a
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2026-02-28 00:32:01 +0000

    test the video id extraction

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,7 +1,19 @@
-from yt_vinyl.ingestion.youtube import youtube_client_object
+from yt_vinyl.ingestion.youtube import youtube_client_object, get_video_ids
 
 
 def test_youtube_client_object(mocker):
     mock_build = mocker.patch("yt_vinyl.ingestion.youtube.build")
     youtube_client_object("fake_api_key")
     mock_build.assert_called_once_with("youtube", "v3", developerKey="fake_api_key")
+
+
+def test_get_video_ids(mocker):
+    mock_youtube = mocker.Mock()
+    mock_youtube.playlistItems().list().execute.return_value = {
+        "items": [
+            {"contentDetails": {"videoId": "abc123"}},
+            {"contentDetails": {"videoId": "def456"}},
+        ]
+    }
+    result = get_video_ids("fake_playlist_id", mock_youtube)
+    assert result == ["abc123", "def456"]
