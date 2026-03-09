d2660494e624ee2f9c147b63291b828a720da1fd
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2026-03-09 00:40:26 +0000

    bronze-silver pipeline fully functional

--- src/yt_vinyl/pipeline.py
+++ src/yt_vinyl/pipeline.py
@@ -11,7 +11,12 @@ from yt_vinyl.storage.storage import (
     create_bronze_table,
     insert_raw_video,
     get_raw_video_db,
+    create_silver_table,
+    insert_silver_track,
+    update_bronze_when_processed,
 )
+
+from yt_vinyl.transform.transform import silver_transform
 
 from yt_vinyl.config import DATABASE_PATH, PLAYLIST_ID
 
@@ -28,9 +33,23 @@ def main():
 
     with establish_connection(DATABASE_PATH) as conn:
         create_bronze_table(conn)
+        conn.commit()
         for entry in raw_video_data:
             insert_raw_video(conn, entry["id"], entry["snippet"])
-        print(get_raw_video_db(conn))
+            conn.commit()
+
+    with establish_connection(DATABASE_PATH) as conn:
+        create_silver_table(conn)
+        conn.commit()
+        pull_from_bronze = get_raw_video_db(conn)
+        transform_bronze = silver_transform(pull_from_bronze)
+        video_id_lookup = {dct["video_id"] for dct in transform_bronze}
+        for video_id in video_id_lookup:
+            for dct in transform_bronze:
+                if dct["video_id"] == video_id:
+                    insert_silver_track(conn, dct)
+            update_bronze_when_processed(conn, video_id)
+            conn.commit()
 
 
 if __name__ == "__main__":
